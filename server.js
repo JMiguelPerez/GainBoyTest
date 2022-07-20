b2d11650cb41f98c881dd7896d351e779a207d79
Chris Lee <monkncheese@gmail.com>
2022-07-20 19:27:07 -0400

    reverted server.js

--- server.js
+++ server.js
@@ -6,29 +6,9 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 
 // set up server
-
-const path = require("path");
-const PORT = process.env.PORT || 5000;
-
 const app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-
-///////////////////////////////////////////////////
-// For Heroku deployment
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
-////////////////////////////////////////////////////
 
 app.use(express.json());
 app.use(cookieParser());
@@ -50,4 +30,3 @@ mongoose.connect(
 app.use("/auth", require("./routers/userRouter"));
 app.use("/customer", require("./routers/customerRouter"));
 // app.use("/signup", require("./routers/registerRouter"));
-
